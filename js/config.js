82587d0d53a6df21828837f929beff6dffe4f6cf
Hannah Ripley <hripley1@jh.edu>
2025-06-11 18:36:27 -0400

    Updated invoke url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ii6mdsysx2.execute-api.us-east-2.amazonaws.com/prod'
     }
 };
